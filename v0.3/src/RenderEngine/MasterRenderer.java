a7823b31ec29bdc50aeee5c1d89069bad7497f34
GaryTheBlobfish <benrupertgrant@gmail.com>
2021-05-18 00:22:50 +0100

    Added v0.3

---
+++ v0.3/src/RenderEngine/MasterRenderer.java
@@ -0,0 +1,87 @@
+package RenderEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.util.vector.Matrix4f;
+
+import Entities.Camera;
+import Entities.Entity;
+import Models.TexturedModel;
+import shaders.StaticShader;
+
+public class MasterRenderer {
+
+	Matrix4f projectionMatrix;
+
+	private static final float FOV = 110f;
+	private static final float NEAR_PLANE = 0.1f;
+	private static final float FAR_PLANE = 10000f;
+	
+	public static StaticShader shader = new StaticShader();
+	EntityRenderer eRenderer = new EntityRenderer();
+	Map<TexturedModel, List<Entity>> entities = new HashMap<TexturedModel, List<Entity>>();
+	
+	public MasterRenderer() {
+		createProjectionMatrix();
+		shader.start();
+		shader.loadProjectionMatrix(projectionMatrix);
+		shader.stop();
+	}
+
+	public void prepare() {
+		GL11.glEnable(GL11.GL_DEPTH_TEST);
+		GL11.glClearColor(0.4f, 0.7f, 1.0f, 1.0f);
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+	}
+
+	public void render(Camera cam) {
+		prepare();
+		// always start shader before rendering
+		shader.start();
+		shader.loadViewMatrix(cam);
+		eRenderer.render(entities);
+		shader.stop();
+		entities.clear();
+	}
+	
+	public void addEntity(Entity entity) {
+		TexturedModel model = entity.getModel();
+		
+		List<Entity> batch = entities.get(model);
+		
+		if (batch != null) {
+			batch.add(entity);
+		} else {
+			List<Entity> newBatch = new ArrayList<Entity>();
+			newBatch.add(entity);
+			entities.put(model, newBatch);
+		}
+	}
+
+	public void createProjectionMatrix() {
+		projectionMatrix = new Matrix4f();
+		
+		float aspectRatio = (float) Display.getWidth() / (float) Display.getHeight();
+		float yScale = (float) (1f / Math.tan(Math.toRadians(FOV/2f)));
+		float xScale = yScale / aspectRatio;
+		float zp = FAR_PLANE + NEAR_PLANE;
+		float zm = FAR_PLANE - NEAR_PLANE;
+		
+		projectionMatrix.m00 = xScale;	// m00 means x and y component in the first (0th) column
+		projectionMatrix.m11 = yScale;
+		projectionMatrix.m22 = -zp/zm;
+		projectionMatrix.m23 = -1;
+		/*
+			m23 means:
+			2 = component x in third (2nd) column
+			3 = component y in fourth (3rd) column
+		*/
+		projectionMatrix.m32 = -(2 * FAR_PLANE * NEAR_PLANE) / zm;
+		projectionMatrix.m33 = 0;
+	}
+}
---
+++ v0.3/src/VoxelEngine/MainGameLoop.java
@@ -0,0 +1,113 @@
+package VoxelEngine;
+
+import static logging.WriteToTXT.clearLogFile;
+import static logging.WriteToTXT.writeToFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
+
+import Entities.Camera;
+import Entities.Entity;
+import Models.RawModel;
+import Models.TexturedModel;
+import RenderEngine.DisplayManager;
+import RenderEngine.Loader;
+import RenderEngine.MasterRenderer;
+import terrain.Chunk;
+import terrain.TerrainGenerator;
+import terrain.voxelTypes.Block;
+import textures.ModelTexture;
+
+public class MainGameLoop {
+
+	public static Loader loader1 = null;
+	
+	static List<Chunk> chunks = Collections.synchronizedList(new ArrayList<Chunk>());
+	static Vector3f camPos = new Vector3f(0,0,0);
+
+	public static void main(String[] args) {
+		try {
+			clearLogFile();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			System.err.println("FILE WRITING ERROR 001: COULD NOT CLEAR LOG FILE");
+		}
+
+		System.out.println("Starting game...");
+		writeToFile("Starting game...");
+		DisplayManager.createDisplay();
+		System.out.println("Creating display...");
+		writeToFile("Creating display...");
+
+		Loader loader = new Loader();
+		loader1 = loader;
+
+		MasterRenderer renderer = new MasterRenderer();
+
+		System.out.println("Created MasterRenderer, Loader and StaticShader...");
+		writeToFile("Created MasterRenderer, Loader and StaticShader");
+		
+		Block block = new Block();
+
+		RawModel model = loader.loadToVAO(block.getVertices(), block.getIndices(), block.getUV()); // create model
+		ModelTexture tex = new ModelTexture(loader.loadTexture("blocks/dirt"));
+		writeToFile("Loaded asset res/textures/dirt.PNG and bound it to a model...");
+		TexturedModel texModel = new TexturedModel(model, tex);
+		
+		Camera cam = new Camera(new Vector3f(0, 0, 0), 0, 0, 0);
+
+		// terrain generating thread
+		new Thread(new Runnable() {
+			public void run() {
+				while(!Display.isCloseRequested()) {
+					TerrainGenerator.generateChunkRF(chunks, texModel, camPos);
+				}
+			}
+		}).start();
+		
+		System.out.println("Entering game loop...");
+		writeToFile("Entering game loop...");
+
+		while (!Display.isCloseRequested()) {
+			cam.move();
+			camPos = cam.getPosition();
+			
+			for(int i = 0; i < chunks.size(); i++) {
+				
+				Vector3f origin = chunks.get(i).getOrigin();
+				
+				int distX = (int) (camPos.x - origin.x);
+				int distZ = (int) (camPos.z - origin.z);
+				
+				if (distX < 0) {
+					distX = -distX;
+				}
+				if (distZ < 0) {
+					distZ = -distZ;
+				}
+				
+				if ((distX <= TerrainGenerator.renderDist) && (distZ <= TerrainGenerator.renderDist)) {
+					for(int j = 0; j < chunks.get(i).getBlocks().size(); j++) {
+						// for every block in the chunk
+						renderer.addEntity(chunks.get(i).getBlocks().get(j));
+					}
+				}
+				TerrainGenerator.setTotalVertexCount(chunks.size() * TerrainGenerator.blockVertexCount);
+			}
+
+			renderer.render(cam);
+			
+			DisplayManager.updateDisplay();
+		}
+
+		System.out.println("Exited game loop, closing display and exiting game...");
+		writeToFile("Exited game loop, closing display and exiting game...");
+		DisplayManager.closeDisplay();
+
+	}
+}
